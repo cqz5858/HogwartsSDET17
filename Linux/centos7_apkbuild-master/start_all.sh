82be2ce5d643905f81a5d279fd1111efe470757e
cqz5858 <chenqinzhao168@163.com>
2021-04-15 16:08:27 +0800

    [centos7]docker+selenium+appium+jenkins+android_app源码打包成apk的环境搭建

---
+++ Linux/centos7_apkbuild-master/start_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bash sh_file/docker.sh
+bash sh_file/python.sh
+bash sh_file/selenium.sh
+bash sh_file/android.sh
+bash sh_file/appium.sh
+bash sh_file/jenkins.sh
+bash sh_file/ssh.sh
+source /root/.bashrc
